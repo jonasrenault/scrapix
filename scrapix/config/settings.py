183252ece7fdd26d81383e9ab17eb1867e2f8d68
Jonas Renault <jonasrenault@gmail.com>
2025-10-28 09:54:30 +0100

    click on thumbnails to extract image urls

--- scrapix/config/settings.py
+++ scrapix/config/settings.py
@@ -1,7 +1,5 @@
 from pathlib import Path
-from typing import ClassVar
 
-from fake_useragent import UserAgent
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -12,14 +10,12 @@ class Settings(BaseSettings):
 
     SCRAPIX_HOME_DIR: Path = Path.home() / ".cache" / "scrapix"
 
-    HEADERS: ClassVar[dict[str, str]] = {
-        "User-Agent": UserAgent().random,
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",  # noqa: E501
-        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "en-US,en;q=0.8",
-        "Connection": "keep-alive",
-    }
+    # Images link text at top of page after a search to switch to image results
+    IMAGES_LINK_TEXT: str = "Images"
+    # CSS selector for thumbnail divs on GoogleImage results page
+    THUMBNAIL_DIV_SELECTOR: str = "div.F0uyec"
+    # CSS classes of source images after having clicked on a thumbnail in results page
+    IMAGE_CLASSES: list[str] = ["n3VNCb", "iPVvYb", "r48jcc", "pT0Scc"]
 
 
 settings = Settings()
--- scrapix/scraper.py
+++ scrapix/scraper.py
@@ -5,8 +5,8 @@ import traceback
 from dataclasses import dataclass
 from pathlib import Path
 
-import PIL
 from fake_useragent import UserAgent
+from PIL import Image
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
@@ -14,13 +14,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from tqdm import tqdm
+
+from scrapix.config.settings import settings
 
 LOGGER = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
-class Image:
+class ImageUrl:
     title: str | None
     url: str
 
@@ -43,9 +44,7 @@ class GoogleImageScraper:
             self.options.add_argument("--headless")
 
         # Set random user agent
-        self.user_agent = UserAgent(
-            os=["Android"], platforms=["mobile"], min_version=120.0
-        ).random
+        self.user_agent = UserAgent(platforms=["desktop"], min_version=120.0).random
         self.options.add_argument(f"--user-agent={self.user_agent}")
 
         # Set options to hide selenium webdriver
@@ -70,7 +69,7 @@ class GoogleImageScraper:
         )
         return repr_str
 
-    def _log_page(self, show: bool = False):
+    def _log_page(self, show: bool = True):
         """
         Log the current page in the webdriver by saving a screenshot and the full dom tree
         to disk.
@@ -85,12 +84,12 @@ class GoogleImageScraper:
                 self.driver.execute_script("return document.documentElement.outerHTML")
             )
         if show:
-            screenshot = PIL.Image.open(screen)
+            screenshot = Image.open(screen)
             screenshot.show()
 
     def _check_recaptcha(self) -> None:
         """
-        Check if page shows a ReCaptcha.
+        Raise a RuntimeError if page shows a ReCaptcha.
 
         Raises:
             RuntimeError: if page shows a ReCaptcha.
@@ -149,48 +148,65 @@ class GoogleImageScraper:
             LOGGER.info("Clicking Images search button")
             time.sleep(1 + random.random())
             WebDriverWait(self.driver, 5).until(
-                EC.element_to_be_clickable((By.LINK_TEXT, "Images"))
+                EC.element_to_be_clickable((By.LINK_TEXT, settings.IMAGES_LINK_TEXT))
             ).click()
             return
         except TimeoutException as e:
             LOGGER.error("Unable to locate `Image` link.")
             raise e
 
-    def _extract_image_url(self) -> Image | None:
-        for class_name in ["n3VNCb", "iPVvYb", "r48jcc", "pT0Scc"]:
-            for image in self.driver.find_elements(By.CLASS_NAME, class_name):
-                url = image.get_attribute("src")
-                if url and "http" in url and "encrypted" not in url:
-                    image_url = Image(image.get_attribute("alt"), url)
-                    return image_url
-        return None
-
-    def _validate_image_url(self, image: Image):
+    def _validate_image_url(self, image: ImageUrl):
         if image.title is None or image.url is None:
             return False
         return True
 
-    def _gather_urls(self, limit: int) -> set[Image]:
+    def _extract_image_url(self) -> ImageUrl | None:
+        """
+        Look for a valid image tag in the page after having clicked on a
+        thumbnail. A valid image tag has one of the known classes, and points
+        to a url which isn't of the type https://encrypted-tbn0.gstatic.com
+
+        Returns:
+            ImageUrl | None: a valid ImageUrl or None if not found.
+        """
+        for class_name in settings.IMAGE_CLASSES:
+            for image in self.driver.find_elements(By.CLASS_NAME, class_name):
+                url = image.get_attribute("src")
+                if url is not None and "http" in url and "encrypted" not in url:
+                    return ImageUrl(image.get_attribute("alt"), url)
+        return None
+
+    def _gather_urls(self, limit: int) -> set[ImageUrl]:
+        """
+        Gather image urls from the results page by clicking on each thumbnail. Clicking
+        on a thumbnail will display the source image on the side panel, allowing us to
+        get its url.
+
+        Args:
+            limit (int): the maximum limit of image urls to fetch.
+
+        Returns:
+            set[ImageUrl]: a set of ImageUrls
+        """
         LOGGER.info("Gathering image urls.")
         time.sleep(2 + random.random())
         # Loop through all the thumbnails, stopping when we found enough images or
         # when results are exhausted.
-        images: set[Image] = set()
+        images: set[ImageUrl] = set()
         visited_thumbnails: list[WebElement] = []
         new_results = True
         while len(images) < limit and new_results:
             # Fetch thumbnails
             thumbnails = self.driver.find_elements(
-                By.XPATH,
-                "//img[starts-with(@id, 'dimg_') and starts-with(@src, 'https')]",
+                By.CSS_SELECTOR, settings.THUMBNAIL_DIV_SELECTOR
             )
 
-            # Check that we have new results
+            # Check that we have new results (thumbnails not clicked on already)
             new_results = len(thumbnails) - len(visited_thumbnails) > 0
             LOGGER.info(f"Found {len(thumbnails) - len(visited_thumbnails)} thumbnails..")
 
             # try to click on every new thumbnail to get the real image behind it
-            for thumbnail in tqdm(thumbnails[len(visited_thumbnails) :]):
+            for thumbnail in thumbnails[len(visited_thumbnails) :]:
                 try:
                     # Using EC.element_to_be_clickable will scroll down to the element
                     # (the element needs to be in the viewport to be clickable).
@@ -198,7 +214,7 @@ class GoogleImageScraper:
                     # on the page.
                     WebDriverWait(self.driver, 3).until(
                         EC.element_to_be_clickable(thumbnail)
-                    )
+                    ).click()
                     time.sleep(0.5 + random.random())
                 except Exception:
                     LOGGER.warning(
@@ -206,14 +222,12 @@ class GoogleImageScraper:
                     )
                     continue
 
-                url = thumbnail.get_attribute("src")
-                if url is None:
+                image = self._extract_image_url()
+                if image is None or not self._validate_image_url(image):
                     continue
 
-                image = Image(thumbnail.get_attribute("alt"), url)
-                if self._validate_image_url(image):
-                    images.add(image)
-                    LOGGER.info(f"Added {image}")
+                images.add(image)
+                LOGGER.info(image)
 
                 if len(images) >= limit:
                     break
@@ -230,7 +244,6 @@ class GoogleImageScraper:
         max_resolution: tuple[int, int] = (2048, 2048),
     ) -> dict[str, str]:
         try:
-            # First, do a regular search for the term, and refuse cookie policy popup.
             LOGGER.info(f"Searching images for {query}")
             self.driver.get(f"https://www.google.com/search?q={query}")
             time.sleep(1 + random.random())

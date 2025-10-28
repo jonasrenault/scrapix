0f9ef07ee1cb7343f231cd3b9aab01f7a2b26b3f
Jonas Renault <jonasrenault@gmail.com>
2025-10-28 15:50:06 +0100

    add methods and tests for image download and check dimensions

--- scrapix/urls.py
+++ scrapix/urls.py
@@ -1,9 +1,33 @@
 import dataclasses
 import inspect
 import json
+import shutil
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, BinaryIO, Self
+from urllib.parse import unquote, urlparse
+
+import requests
+from fake_useragent import UserAgent
+from PIL import Image
+
+
+def fake_headers() -> dict[str, str]:
+    """
+    Generate fake http headers.
+
+    Returns:
+        dict[str, str]: fake headers.
+    """
+    user_agent = UserAgent(platforms=["desktop"], min_version=120.0).random
+    return {
+        "User-Agent": user_agent,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",  # noqa: E501
+        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+        "Accept-Encoding": "none",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Connection": "keep-alive",
+    }
 
 
 @dataclass(frozen=True)
@@ -30,6 +54,41 @@ class ImageUrl:
             }
         )
         return instance
+
+    def check_dimensions(
+        self, min_resolution: tuple[int, int], max_resolution: tuple[int, int]
+    ) -> bool:
+        """
+        Check whether image size is between min_resolution and max_resolution.
+
+        Args:
+            min_resolution (tuple[int, int]): minimum resolution.
+            max_resolution (tuple[int, int]): maximum resolution.
+
+        Returns:
+            bool: True if image dimensions are between min_resolution and max_resolution.
+        """
+        response = requests.get(self.url, stream=True, headers=fake_headers())
+        response.raise_for_status()
+        with Image.open(response.raw) as image:  # type: ignore
+            return image.size is None or (
+                min_resolution[0] <= image.size[0] <= max_resolution[0]
+                and min_resolution[1] <= image.size[1] <= max_resolution[1]
+            )
+
+    def download(self, save_dir: Path):
+        """
+        Download image from url into save_dir. Image filename is taken
+        from url.
+
+        Args:
+            save_dir (Path): output directory where image is saved.
+        """
+        response = requests.get(self.url, stream=True, headers=fake_headers())
+        response.raise_for_status()
+        file = save_dir / get_filename(self.url)
+        with open(file, "wb") as f:
+            shutil.copyfileobj(response.raw, f)
 
 
 def read_urls(uri: BinaryIO | Path) -> set[ImageUrl]:
@@ -62,3 +121,19 @@ def write_urls(urls: set[ImageUrl], file: Path) -> None:
     json_data = [dataclasses.asdict(url) for url in urls]
     with open(file, "w") as f:
         json.dump(json_data, f, indent=2)
+
+
+def get_filename(url: str) -> str:
+    """
+    Get filename from image url.
+
+    Args:
+        url (str): the image's url
+
+    Returns:
+        str: the image's filename.
+    """
+    parsed_url = urlparse(url)
+    path = parsed_url.path
+    filename = unquote(Path(path).name)
+    return filename
--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,4 +1,10 @@
-from scrapix.urls import ImageUrl
+from pathlib import Path
+
+import responses
+
+from scrapix.urls import ImageUrl, get_filename
+
+ROOT_DIR = Path(__file__).parent.parent
 
 
 def test_urls_can_be_created():
@@ -8,3 +14,33 @@ def test_urls_can_be_created():
     image_url = ImageUrl(title, url)
     assert image_url.title == title
     assert image_url.url == url
+
+
+def test_get_filename():
+    url = "https://cdn.store-factory.com/www.reptile-paradise.fr/content/product%20image.jpg?v=1720559822"
+    filename = get_filename(url)
+    assert filename == "product image.jpg"
+
+    url = "https://www.imagesdoc.com/wp-content/uploads/sites/33/2018/10/AdobeStock_67152957-e1540977088518.jpeg"
+    filename = get_filename(url)
+    assert filename == "AdobeStock_67152957-e1540977088518.jpeg"
+
+
+@responses.activate
+def test_check_dimensions():
+    with open(ROOT_DIR / "resources" / "images" / "scraper.webp", "rb") as image:
+        mock_url = "http://example.org/test/test-image.jpg"
+        responses.get(
+            mock_url,
+            body=image.read(),
+            status=200,
+            content_type="image/jpeg",
+            stream=True,
+        )
+
+    image_url = ImageUrl("A nice scraper", mock_url)
+
+    # mock image has dimension 452 x 452
+    assert image_url.check_dimensions((20, 20), (1200, 1200))
+    assert not image_url.check_dimensions((640, 640), (1200, 1200))
+    assert not image_url.check_dimensions((20, 20), (320, 320))

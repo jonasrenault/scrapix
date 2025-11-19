005b282f808362dfef9da6fbf30c6ba2eb61a01c
Jonas Renault <jonasrenault@gmail.com>
2025-11-19 21:37:33 +0100

    check url files exists in test

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -7,8 +7,12 @@ from scrapix.scraper import GoogleImageScraper
 
 @pytest.mark.asyncio
 async def test_headless_google_scraping(tmp_path: Path):
-    scraper = await GoogleImageScraper.create(tmp_path, True)
+    urls_file = "my_duck_urls.json"
+    scraper = await GoogleImageScraper.create(
+        tmp_path, headless=True, urls_file=urls_file
+    )
     urls = await scraper.get_image_urls(
         "duck", limit=2, skip=0, keywords=["rubber", "toy"]
     )
     assert len(urls) == 2
+    assert tmp_path.joinpath(urls_file).exists()

5a0aeaf317d698af4ba0bfded32c60b5ae9185c3
Jonas Renault <jonasrenault@gmail.com>
2025-11-25 10:02:37 +0100

    fix results display issue in gui

--- scrapix/gui/app.py
+++ scrapix/gui/app.py
@@ -26,6 +26,36 @@ st.markdown(
 )
 
 
+def clear_state():
+    """
+    Initialize / clear session state to default values.
+    """
+    if "errors" not in st.session_state:
+        st.session_state.errors = []
+    if "urls" not in st.session_state:
+        st.session_state.urls = []
+
+
+clear_state()
+
+
+with st.container():
+    for error in st.session_state.errors:
+        st.error(error)
+
+results_placeholder = st.empty()
+results_height = 400
+results_container = results_placeholder.container(
+    height=results_height if st.session_state.urls else "content"
+)
+results_container.empty()
+urls_cols = results_container.columns(3)
+for idx, url in enumerate(st.session_state.urls):
+    urls_cols[idx % len(urls_cols)].image(url.url, caption=url.title)
+
+parameters_container = st.container()
+
+
 async def scrape_urls(
     query: str,
     save_dir: Path,
@@ -45,155 +75,137 @@ async def scrape_urls(
     async for url in scraper.get_image_urls(
         query, limit=limit, skip=skip, keywords=keywords, min_res=min_res, max_res=max_res
     ):
+        st.session_state.urls.append(url)
         cols[count % len(cols)].image(url.url, caption=url.title)
         count += 1
-        pbar.progress(count / limit)
+        pbar.progress(count / limit, text=progress_text + f"\t{count}/{limit}")
 
     pbar.empty()
 
 
-def on_scrape(
-    query: str,
-    save_dir: str,
-    keywords: list[str],
-    limit: int,
-    skip: int,
-    min_width: int,
-    max_width: int,
-    min_height: int,
-    max_height: int,
-    headless: bool,
-    errors_placeholder: DeltaGenerator,
-    results_placeholder: DeltaGenerator,
-):
-    error = False
-    error_container = errors_placeholder.container()
-    results_container = results_placeholder.container()
+def on_scrape() -> None:
+    st.session_state.errors = []
+    st.session_state.urls = []
+    results_placeholder.empty()
+    results_container = results_placeholder.container(height=results_height)
 
-    if not query:
-        error_container.error("Please enter a valid search query.")
-        error = True
+    if not st.session_state.query:
+        st.session_state.errors.append("Please enter a valid search query.")
 
-    output = Path(save_dir)
-    if not output.is_dir():
-        error_container.error(f"{output} is not a valid directory.")
-        error = True
-
-    if error:
+    if st.session_state.errors:
         return
 
+    output = Path(st.session_state.save_dir)
     min_res: tuple[int, int] | None = None
     max_res: tuple[int, int] | None = None
-    if min_width > 0 and min_height > 0:
-        min_res = (min_width, min_height)
-    if max_width > 0 and max_height > 0:
-        max_res = (max_width, max_height)
+    if st.session_state.min_width > 0 and st.session_state.min_height > 0:
+        min_res = (st.session_state.min_width, st.session_state.min_height)
+    if st.session_state.max_width > 0 and st.session_state.max_height > 0:
+        max_res = (st.session_state.max_width, st.session_state.max_height)
 
     asyncio.run(
         scrape_urls(
-            query,
-            output.joinpath(query),
-            keywords,
-            limit,
-            skip,
+            st.session_state.query,
+            output.joinpath(st.session_state.query),
+            st.session_state.keywords,
+            st.session_state.limit,
+            st.session_state.skip,
             min_res,
             max_res,
-            headless,
+            st.session_state.headless,
             results_container,
         )
     )
 
-
-def display_scrape_parameters(
-    parameters_container: DeltaGenerator,
-    errors_placeholder: DeltaGenerator,
-    results_placeholder: DeltaGenerator,
-):
-    query = parameters_container.text_input(
-        "Google Search query", key="query", placeholder="Search for images on Google ..."
-    )
-    save_dir = parameters_container.text_input(
-        "Save directory",
-        value=settings.SCRAPIX_HOME_DIR,
-        help="Directory where images and urls will be saved.",
-    )
-    keywords = parameters_container.multiselect(
-        "Keywords",
-        [],
-        accept_new_options=True,
-        help="Keywords to exclude in image urls or titles.",
-    )
-
-    col1, col2 = parameters_container.columns(2)
-    with col1:
-        limit = st.number_input(
-            "Limit", min_value=1, value=10, help="Max number of images to fetch."
-        )
-    with col2:
-        skip = st.number_input(
-            "Skip", min_value=0, help="Number of images in search results to skip."
-        )
-
-    col1, col2, col3, col4 = parameters_container.columns(4)
-    with col1:
-        min_width = st.number_input(
-            "Min width",
-            min_value=0,
-            max_value=4096,
-            help="Minimum width of images in pixels.",
-        )
-    with col2:
-        min_height = st.number_input(
-            "Min height",
-            min_value=0,
-            max_value=4096,
-            help="Minimum height of images in pixels.",
-        )
-    with col3:
-        max_width = st.number_input(
-            "Max width",
-            min_value=0,
-            max_value=4096,
-            help="Maximum width of images in pixels.",
-        )
-    with col4:
-        max_height = st.number_input(
-            "Max height",
-            min_value=0,
-            max_value=4096,
-            help="Maximum height of images in pixels.",
-        )
-
-    col1, col2 = parameters_container.columns(2)
-    with col1:
-        headless = st.checkbox(
-            "Headless",
-            value=True,
-            help="Run scraping in headless mode, without browser GUI.",
-        )
-    with col2:
-        st.button(
-            "Scrape !",
-            icon="🚀",
-            on_click=on_scrape,
-            args=(
-                query,
-                save_dir,
-                keywords,
-                limit,
-                skip,
-                min_width,
-                min_height,
-                max_width,
-                max_height,
-                headless,
-                errors_placeholder,
-                results_placeholder,
-            ),
-        )
+    results_placeholder.empty()
 
 
-errors_placeholder = st.empty()
-parameters_container = st.expander("Scrape parameters", expanded=True)
-results_placeholder = st.empty()
-display_scrape_parameters(parameters_container, errors_placeholder, results_placeholder)
+def display_scrape_parameters() -> None:
+    with parameters_container.form("input_form"):
+        st.text_input(
+            "Google Search query",
+            key="query",
+            placeholder="Search for images on Google ...",
+        )
+        st.text_input(
+            "Save directory",
+            key="save_dir",
+            value=settings.SCRAPIX_HOME_DIR,
+            help="Directory where images and urls will be saved.",
+        )
+        st.multiselect(
+            "Keywords",
+            [],
+            key="keywords",
+            accept_new_options=True,
+            help="Keywords to exclude in image urls or titles.",
+        )
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.number_input(
+                "Limit",
+                key="limit",
+                min_value=1,
+                value=5,
+                help="Max number of images to fetch.",
+            )
+        with col2:
+            st.number_input(
+                "Skip",
+                key="skip",
+                min_value=0,
+                help="Number of images in search results to skip.",
+            )
+
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.number_input(
+                "Min width",
+                key="min_width",
+                min_value=0,
+                max_value=4096,
+                help="Minimum width of images in pixels.",
+            )
+        with col2:
+            st.number_input(
+                "Min height",
+                key="min_height",
+                min_value=0,
+                max_value=4096,
+                help="Minimum height of images in pixels.",
+            )
+        with col3:
+            st.number_input(
+                "Max width",
+                key="max_width",
+                min_value=0,
+                max_value=4096,
+                help="Maximum width of images in pixels.",
+            )
+        with col4:
+            st.number_input(
+                "Max height",
+                key="max_height",
+                min_value=0,
+                max_value=4096,
+                help="Maximum height of images in pixels.",
+            )
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.checkbox(
+                "Headless",
+                key="headless",
+                value=True,
+                help="Run scraping in headless mode, without browser GUI.",
+            )
+        with col2:
+            st.form_submit_button(
+                "Scrape !",
+                icon="🚀",
+                on_click=on_scrape,
+            )
+
+
+display_scrape_parameters()

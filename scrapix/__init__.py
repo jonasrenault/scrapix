c53a9f2d379608543f797715bda8db435b2cf6ed
Jonas Renault <jonasrenault@gmail.com>
2025-11-25 15:07:44 +0100

    update version to 2.1.0

--- scrapix/__init__.py
+++ scrapix/__init__.py
@@ -1,6 +1,6 @@
 from scrapix.scraper import GoogleImageScraper
 from scrapix.urls import ImageUrl
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 __all__ = ["__version__", "GoogleImageScraper", "ImageUrl"]
